5189089a9eb5035844579f6796d3d3eec22cac13
Karthik Kasubha <karthik.kasubhaforyou@hotmail.com>
2025-05-30 20:00:48 +1200

    Add project files.

---
+++ StringProblems.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PracticeProblems
+{
+    public class StringProblems
+    {
+        // Given a string, print the count of upper case characters
+
+        public int CountUpper(string input)
+        {
+            int count = 0;
+            int a = 'A';
+            int z = 'Z';
+
+            // shorter way input.Count(c => c >= a && c <= z);
+            foreach (char c in input)
+            {
+                int C = c;
+                if (C >= a &&  C <= z)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+           
+        }
+    }
+}
