aaaa5fcd590837dd61c491280b488010ae6290d2
Karthik Kasubha <karthik.kasubhaforyou@hotmail.com>
2025-05-30 20:19:58 +1200

    Unit tests Project

---
+++ PracticeProblems.Test/StringProblemsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace PracticeProblems.Test
+{
+    public class StringProblemsTests
+    {
+     
+        //public void CountUpper_ShouldReturnCorrectCount_ForSingleInput(int input, int output)
+        //{
+        //    StringProblems problems = new StringProblems();
+        //    int result = problems.CountUpper("kArTHiK");
+        //    Assert.Equal(4,result);
+        //}
+
+        [Theory]
+        [InlineData("kArTHiK", 4)]
+        [InlineData("HelloWorld", 2)]
+        [InlineData("12345", 0)]
+        [InlineData("ALLCAPS", 7)]
+        public void CountUpper_ShouldReturnCorrectCount_ForMultipleInputs(string input, int expectedOutput)
+        {
+            // Arrange
+            StringProblems problems = new StringProblems();
+
+            // Act
+            int actualOutput = problems.CountUpper(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
